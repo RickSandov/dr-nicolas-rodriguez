57ca345d1b3eb3c7e7e0c93968e4b786c5424284
Ricardo Sandoval <i.s.ricardo.sandoval@gmail.com>
2023-05-14 00:02:45 -0600

    NAvbar update

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import { NavLinks } from './NavLinks'
 export const Navbar = () => {
     return (
         <nav className={`fixed z-50 left-2 right-2 top-3 bg-white rounded-full py-3 px-4 mx-auto max-w-[1200px] shadow-md flex justify-between items-center `} >
-            <div className="flex gap-2 md:gap-3 items-center">
+            <a href='/#inicio' className="flex gap-2 md:gap-3 items-center">
                 <Logo />
                 <LogoText />
-            </div>
+            </a>
             <NavLinks />
         </nav>
     )
