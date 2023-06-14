8cad6b7658d382e4b750234d98adfe6523d3924e
Ricardo Sandoval <i.s.ricardo.sandoval@gmail.com>
2023-06-13 21:58:27 -0600

    test

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,11 @@
-import { api } from '@/api';
 import { MyCalendar } from '@/components/admin/Calendar/Calendar';
 import { ContactFormsList } from '@/components/pages/admin/ContactFormsList';
-import { IParsedContactForm } from '@/interfaces';
 import React from 'react'
 
 const Page = async () => {
-    const req = await fetch('https://dr-nicolas-rodriguez.vercel.app/api/contact');
-    // const req = await fetch('http://localhost:3000/api/contact');
+    const req = await fetch('https://dr-nicolas-rodriguez.vercel.app/api/contact', { next: { revalidate: 10 } });
     const contactFormsArray = await req.json();
-
+    // const req = await fetch('http://localhost:3000/api/contact', { next: { revalidate: 10 } });
     return (
         <div>
             {/* Crear componente */}
