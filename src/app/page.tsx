90061efd6bbd244d8792b2a09512f6b8d691eae8
Ricardo Sandoval <i.s.ricardo.sandoval@gmail.com>
2023-07-31 16:27:54 -0600

    Metadata description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,27 @@ import { HomePage } from '@/components/pages/HomePage'
 import Image from 'next/image'
 import { Metadata } from 'next'
 
+const description = 'Atención médica especializada en salud femenina, con enfoque preventivo y soluciones personalizadas para cada etapa de tu vida.';
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.drnicolasrodriguez.com'),
   title: ``,
-  description: ``,
+  description,
   openGraph: {
+    description,
     type: `website`,
     title: `Dr. Nicolás Rodríguez Luna. Ginecología y Obstetricia. Clínica de Displasias. Colposcopía.`,
     countryName: 'México',
     locale: `es-MX`,
     url: `https://www.drnicolasrodriguez.com/`,
     images: [
-      `https://www.drnicolasrodriguez.com/`,
-      `https://www.drnicolasrodriguez.com/`,
-
-      // {
-      //   url: 'https://www.drnicolasrodriguez.com/',
-      //   alt: 'https://drnicolasrodriguez.com/',
-      //   type: 'image/png',
-      //   width: '',
-      //   height: '',
-      // }
+      `${process.env.NEXT_PUBLIC_BASE_URL || 'https://www.drnicolasrodriguez.com'}/_next/image?url=%2Fconsultorio.jpg&w=2048&q=75`,
+      `${process.env.NEXT_PUBLIC_BASE_URL || 'https://www.drnicolasrodriguez.com'}/_next/image?url=%2Fconsultorio.jpg&w=2048&q=75`,
     ]
   },
   // verification: {
   //   google: '',
   // }
 }
-
-//       <meta name="twitter:card" content="summary_large_image" />
-//       <meta name="twitter:site" content="@site" />
-//       <meta name="twitter:creator" content="@creator" />
-//       <meta name="twitter:title" content="My Website" />
-//       <meta name="twitter:description" content="My Website Description" />
-//       <meta name="twitter:image" content="https://example.com/og.png" /> */
-
 export default function Home() {
   return (
     <>
